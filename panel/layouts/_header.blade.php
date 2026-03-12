1f197579e8024867479e9e90be9057f72ae75ecc
Abba Gueime Ali <77186890+abbagueimeali@users.noreply.github.com>
2026-03-12 10:14:24 +0100

    Add files via upload

---
+++ panel/layouts/_header.blade.php
@@ -0,0 +1,143 @@
+<!-- Navbar -->
+
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+      @php
+        //$AllChatUserCount = App\Models\ChatModel::getAllChatUserCount()
+      @endphp
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item ">
+        {{--<a class="nav-link" href="{{ url('chat') }}">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge"></span>
+        </a>--}}
+        <div class="navbar-nav font-weight-bold mx-auto py-0">
+            <a href="{{ url('') }}" class="btn btn-primary px-4">Home</a>
+        </div>
+      </li>
+      
+
+
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="javascript:;" class="brand-link" style="text-align: center">
+      @if(!empty($getHeaderSetting->getLogo()))
+      <img src="{{ $getHeaderSetting->getLogo() }}" style="width: auto; height: 60px; border-radius: 5px;"> 
+      @else
+      <span class="brand-text font-weight-light" style="font-size: bold !important;font-size: 20px;">School</span>
+      @endif
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img style="height: 40px; width: 40px;" src="{{ $getUser->getProfile() }}" class="img-circle elevation-2" alt="{{  Auth::user()->name }}">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{  Auth::user()->name }}</a>
+        </div>
+      </div>
+
+
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+           
+                <li class="nav-item">
+                    <a href="{{ url('panel/dashboard') }}" class="nav-link @if(Request::segment(2) == 'dashboard') active @endif">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>
+                        Dashboard
+                    </p>
+                    </a>
+                </li>
+                @if(Auth::user()->is_admin == 1)
+                <li class="nav-item">
+                    <a href="{{ url('panel/user/list') }}" class="nav-link @if(Request::segment(2) == 'user') active @endif">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        Users
+                    </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ url('panel/category/list') }}" class="nav-link @if(Request::segment(2) == 'category') active @endif">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        Category
+                    </p>
+                    </a>
+                </li>
+                @endif
+                <li class="nav-item">
+                    <a href="{{ url('panel/blog/list') }}" class="nav-link @if(Request::segment(2) == 'blog') active @endif">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        Blog
+                    </p>
+                    </a>
+                </li>
+                @if(Auth::user()->is_admin == 1)
+                <li class="nav-item">
+                    <a href="{{ url('panel/page/list') }}" class="nav-link @if(Request::segment(2) == 'page') active @endif">
+                    <i class="nav-icon far fa-user"></i>
+                    <p>
+                        Page
+                    </p>
+                    </a>
+                </li>
+                @endif
+                
+                <li class="nav-item">
+                    <a href="{{ url('panel/change-password') }}" class="nav-link @if(Request::segment(2) == 'change-password') active @endif">
+                    <i class="nav-icon fas fa-lock"></i>
+                    <p>
+                        Change Password
+                    </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ url('panel/account-setting') }}" class="nav-link @if(Request::segment(2) == 'account-setting') active @endif">
+                    <i class="nav-icon fas fa-cog"></i>
+                    <p>
+                        Account Setting
+                    </p>
+                    </a>
+                </li>
+                
+
+
+          <li class="nav-item">
+            <a href="{{ url('logout') }}" class="nav-link">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+                Logout
+              </p>
+            </a>
+          </li>
+
+
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
